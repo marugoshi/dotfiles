56e26f698488ddbfe56a2e5e8ba03b2f537d78ae
Makoto Tokuyama <marugoshi@me.com>
2011-08-06 19:55:32 +0900

    add vim colors and change method to add files under sub directory. it's dirty:(

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ for global_dir in $global_dirs ; do
             printf "    Create $create_dir...done\n"
             mkdir -p $create_dir
           else
-            printf "    $created_dir is already exists in your home directory. skipped.\n"
+            printf "    $create_dir is already exists in your home directory. skipped.\n"
           fi
         fi
       done
@@ -66,47 +66,82 @@ for orig_file in $common_files $os_files ; do
         orig_path=$orig_sub_dir_file # ex) /rc/dotfiles/common/vim/ftplugin/ruby.vim
         target_file=".$tmp_path" # ex) ruby.vim or .vim/ftplugin/ruby.vim
         target_path="$HOME/.$tmp_path" # ex) /Users/tokuyama/.vim/ftplugin/ruby.vim
+
+        if [ -f $target_path ] ; then
+          printf "    $target_file is already exists in your home directory. Override?: [yn]\n"
+          read flag
+          case $flag in
+            'y')
+              rm $target_path
+              printf "      Copy file or create symbolic link?: [cs]\n"
+              read type
+              case $type in
+                'c')
+                  cp $orig_path $target_path
+                  ;;
+                's')
+                  ln -s $orig_path $target_path
+                  ;;
+              esac
+              ;;
+            *)
+              printf "    skiped.\n"
+              ;;
+          esac
+        else
+          printf "    Copy $target_file or create symbolic link?: [cs]\n"
+          read type
+          case $type in
+            'c')
+              cp $orig_path $target_path
+              ;;
+            's')
+              ln -s $orig_path $target_path
+              ;;
+          esac
+        fi
       fi
     done
   fi
+
   if [ -f $orig_file ] ; then
     orig_path=$orig_file # ex) /rc/dotfiles/common/vimrc
     target_file=".${orig_path##*/}" # ex) .vimrc
     target_path="$HOME/$target_file" # ex) /Users/tokuyama/.vimrc
-  fi
 
-  if [ -f $target_path ] ; then
-    printf "    $target_file is already exists in your home directory. Override?: [yn]\n"
-    read flag
-    case $flag in
-      'y')
-        rm $target_path
-        printf "      Copy file or create symbolic link?: [cs]\n"
-        read type
-        case $type in
-          'c')
-            cp $orig_path $target_path
-            ;;
-          's')
-            ln -s $orig_path $target_path
-            ;;
-        esac
-        ;;
-      *)
-        printf "    skiped.\n"
-        ;;
-    esac
-  else
-    printf "    Copy $target_file or create symbolic link?: [cs]\n"
-    read type
-    case $type in
-      'c')
-        cp $orig_path $target_path
-        ;;
-      's')
-        ln -s $orig_path $target_path
-        ;;
-    esac
+    if [ -f $target_path ] ; then
+      printf "    $target_file is already exists in your home directory. Override?: [yn]\n"
+      read flag
+      case $flag in
+        'y')
+          rm $target_path
+          printf "      Copy file or create symbolic link?: [cs]\n"
+          read type
+          case $type in
+            'c')
+              cp $orig_path $target_path
+              ;;
+            's')
+              ln -s $orig_path $target_path
+              ;;
+          esac
+          ;;
+        *)
+          printf "    skiped.\n"
+          ;;
+      esac
+    else
+      printf "    Copy $target_file or create symbolic link?: [cs]\n"
+      read type
+      case $type in
+        'c')
+          cp $orig_path $target_path
+          ;;
+        's')
+          ln -s $orig_path $target_path
+          ;;
+      esac
+    fi
   fi
 done
 
