d8c41ba7cd02bdc659548626d7ee4603c76e3074
Makoto Tokuyama <tokuyama@gardia.jp>
2019-12-19 14:10:37 +0900

    add zsh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@
 
 # initialize
 support_os="redhat debian osx"
+support_sh="bash zsh"
 
 # start
 printf "Installing dotfiles...\n"
@@ -18,12 +19,28 @@ read selected_os
 for os in $support_os ; do
   if [ -n "$selected_os" ] ; then
     if [ $selected_os = $os ] ; then
-      selected=1
+      selected_os_flg=1
     fi
   fi
 done
-if [ -z "$selected" ] ; then
+if [ -z "$selected_os_flg" ] ; then
   printf "  You should type OS.\n"
+  exit 1
+fi
+printf "\n"
+
+# set sh
+printf "  Type sh from below: [$support_sh]\n"
+read selected_sh
+for sh in $support_sh ; do
+  if [ -n "$selected_sh" ] ; then
+    if [ $selected_sh = $sh ] ; then
+      selected_sh_flg=1
+    fi
+  fi
+done
+if [ -z "$selected_sh_flg" ] ; then
+  printf "  You should type sh.\n"
   exit 1
 fi
 printf "\n"
@@ -31,10 +48,11 @@ printf "\n"
 # create symbolic link
 printf "  Create files under your home directory...\n"
 common_files="`pwd`/common/*"
-os_files="`pwd`/$selected_os/*"
-for orig_file in $common_files $os_files ; do
+sh_common_files="`pwd`/$selected_sh/common/*"
+sh_os_files="`pwd`/$selected_sh/$selected_os/*"
+for orig_file in $common_files $sh_common_files $sh_os_files ; do
   if [ -f $orig_file ] ; then
-    orig_path=$orig_file # ex) /rc/dotfiles/common/vimrc
+    orig_path=$orig_file # ex) /rc/dotfiles/zsh/common/vimrc
     target_file=".${orig_path##*/}" # ex) .vimrc
     target_path="$HOME/$target_file" # ex) /Users/tokuyama/.vimrc
 
